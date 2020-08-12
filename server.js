f208baa1bf7e3dabe93711c419a670e7ed9783b1
israr2018 <israr.qr@gmail.com>
2020-08-12 09:15:44 +0300

    Fixed on server  file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+// app.use(express.static(path.join(__dirname+'/dist/mcza6')));
+app.use(express.static('../build'));
+const port=process.env.PORT||8080;
+//app.use(cors({credentials: true, origin: true}));
+app.get('/*',function(req,res){
+  res.send("Hello World");
+  //res.sendFile('./build/index.html');
+});
+
+// app.get('/',function(req,res) {
+//     res.send("Welcome to the strip apis ");
+// });
+app.listen(port,function () {
+    console.log("strip  restfull api are running on port:"+port);
+
+});
